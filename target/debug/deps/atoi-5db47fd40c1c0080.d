194b91171b4cf41aca301649f9d5206d846c5f21
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:57:32 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/atoi-5db47fd40c1c0080.d
@@ -0,0 +1,7 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/atoi-5db47fd40c1c0080.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atoi-2.0.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libatoi-5db47fd40c1c0080.rlib: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atoi-2.0.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libatoi-5db47fd40c1c0080.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atoi-2.0.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atoi-2.0.0/src/lib.rs:
---
+++ target/debug/deps/lexical_core-64e84dbd326bd1bb.d
@@ -0,0 +1,7 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/lexical_core-64e84dbd326bd1bb.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lexical-core-1.0.6/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/liblexical_core-64e84dbd326bd1bb.rlib: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lexical-core-1.0.6/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/liblexical_core-64e84dbd326bd1bb.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lexical-core-1.0.6/src/lib.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lexical-core-1.0.6/src/lib.rs:
