8c0ef77346e7b92557c9baa0438accceaee5847f
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 10:50:16 +0800

    Auto-commit: Project implementation in progress

--- crates/node/src/middleware.rs
+++ crates/node/src/middleware.rs
@@ -149,7 +149,7 @@ mod tests {
         let next = |req: Request<Body>| async move {
             Ok::<_, Infallible>(Response::new(req.into_body()))
         };
-        let wrapped = axum::middleware::from_fn::<_, _, _, _>(auth_middleware);
+        let wrapped = axum::middleware::from_fn::<_, _, _>(auth_middleware);
         let _ = wrapped;
     }
 
@@ -161,7 +161,7 @@ mod tests {
         let next = |req: Request<Body>| async move {
             Ok::<_, Infallible>(Response::new(req.into_body()))
         };
-        let wrapped = axum::middleware::from_fn::<_, _, _, _>(auth_middleware);
+        let wrapped = axum::middleware::from_fn::<_, _, _>(auth_middleware);
         let _ = wrapped;
     }
 
