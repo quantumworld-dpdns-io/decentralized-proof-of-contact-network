b3dc050c9e700765d94b35bf18c052f1f65160d2
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:58:15 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/serde_spanned-ec477d3887145212.d
@@ -0,0 +1,8 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/serde_spanned-ec477d3887145212.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libserde_spanned-ec477d3887145212.rlib: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libserde_spanned-ec477d3887145212.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs:
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs:
