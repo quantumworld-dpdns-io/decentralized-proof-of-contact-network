10a93f4e58688956ef80b607dfef577f385376eb
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:27:21 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/rust_embed_utils-e1fe3fb90b6af646.d
@@ -0,0 +1,7 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/rust_embed_utils-e1fe3fb90b6af646.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-embed-utils-8.11.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/librust_embed_utils-e1fe3fb90b6af646.rlib: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-embed-utils-8.11.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/librust_embed_utils-e1fe3fb90b6af646.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-embed-utils-8.11.0/src/lib.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-embed-utils-8.11.0/src/lib.rs:
