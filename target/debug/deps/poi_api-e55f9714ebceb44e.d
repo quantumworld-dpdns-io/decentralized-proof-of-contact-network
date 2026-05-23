49fc4e6381907c71856b8f0a81c1dec0a25ff89d
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:52:19 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/poi_api-e55f9714ebceb44e.d
@@ -0,0 +1,5 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/poi_api-e55f9714ebceb44e.d: crates/api/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libpoi_api-e55f9714ebceb44e.rmeta: crates/api/src/lib.rs
+
+crates/api/src/lib.rs:
