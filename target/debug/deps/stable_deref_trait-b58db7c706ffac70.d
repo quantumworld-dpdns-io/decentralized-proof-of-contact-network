35fa8caf70483b4bcd843dc992790224ae8a0e08
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:22:44 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/stable_deref_trait-b58db7c706ffac70.d
@@ -0,0 +1,7 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/stable_deref_trait-b58db7c706ffac70.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libstable_deref_trait-b58db7c706ffac70.rlib: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libstable_deref_trait-b58db7c706ffac70.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs:
