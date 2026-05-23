2f62c2a0479b8b164301d5c13871b1d70c6dd01e
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:23:33 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/futures_executor-4a9fe7051ca80582.d
@@ -0,0 +1,7 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/futures_executor-4a9fe7051ca80582.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/local_pool.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/enter.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libfutures_executor-4a9fe7051ca80582.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/local_pool.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/enter.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/lib.rs:
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/local_pool.rs:
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-executor-0.3.32/src/enter.rs:
