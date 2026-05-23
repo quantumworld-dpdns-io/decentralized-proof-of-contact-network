ffb0555446576c4ee67bee408663a1b6e2708b0b
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-23 09:15:29 +0800

    Auto-commit: Project implementation in progress

---
+++ target/debug/deps/fnv-333b354419202b14.d
@@ -0,0 +1,5 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/fnv-333b354419202b14.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fnv-1.0.7/lib.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libfnv-333b354419202b14.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fnv-1.0.7/lib.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fnv-1.0.7/lib.rs:
---
+++ target/debug/deps/zeroize-adb9b8b83dfee096.d
@@ -0,0 +1,6 @@
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/zeroize-adb9b8b83dfee096.d: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs
+
+/Users/dennis_leedennis_lee/Documents/GitHub/decentralized-proof-of-contact-network/target/debug/deps/libzeroize-adb9b8b83dfee096.rmeta: /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs /Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs
+
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs:
+/Users/dennis_leedennis_lee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs:
